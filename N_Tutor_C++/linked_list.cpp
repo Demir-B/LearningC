8fd8bf2125778a5becd3d2c40e4485054855da59
Nikolai Rozanov <nikolai.rozanov@gmail.com>
2024-12-05 18:38:15 +0000

    renamed folder to avoid conflicts in the future

--- N_Tutor_C++/linked_list.cpp
+++ N_Tutor_C++/linked_list.cpp
@@ -55,21 +55,21 @@ int main(){
     // twoDpoint origin = {0,0};
     // cout << origin.x;
 
-    twoDpoint array_of_points[3] = {{0,0},{1,2},{1,3}};
+//     twoDpoint array_of_points[3] = {{0,0},{1,2},{1,3}};
 
-    cout << array_of_points << endl;
-    cout << array_of_points + 1 << endl;
+//     cout << array_of_points << endl;
+//     cout << array_of_points + 1 << endl;
 
-    cout << array_of_points[2].x << " " << array_of_points[2].y << endl;
-    cout << (*(array_of_points+2)).x << " " << (*(array_of_points + 2)).y << endl;
-    cout << (array_of_points + 2) -> x << " " << (array_of_points + 2) -> y << endl;
+//     cout << array_of_points[2].x << " " << array_of_points[2].y << endl;
+//     cout << (*(array_of_points+2)).x << " " << (*(array_of_points + 2)).y << endl;
+//     cout << (array_of_points + 2) -> x << " " << (array_of_points + 2) -> y << endl;
     
-    node element1, element2;
-    element1.next_node = &element2;
-    element2.value = 5;
+//     node element1, element2;
+//     element1.next_node = &element2;
+//     element2.value = 5;
 
-   cout << (*element1.next_node).value << endl;
-   cout << element1.next_node -> value << endl;
+//    cout << (*element1.next_node).value << endl;
+//    cout << element1.next_node -> value << endl;
 
 }
 
