fd625c19bd405f1a93f621e5bbe14faba6391543
Demir-B <demir.bibezic@gmail.com>
2024-12-11 11:43:44 +0000

    Making Linked List in C++

--- C++/linked_list.cpp
+++ C++/linked_list.cpp
@@ -2,35 +2,14 @@
 
 using namespace std;
 
-struct twoDpoint{
-    float x;
-    float y;
-};
-
 struct node{
     int value;
     node * next_node;
 };
 
 int main() {
-    twoDpoint origin = {0,0};
-    cout << origin.x;
 
-    twoDpoint array_of_points[3] = {{0,0},{1,2},{3,4}};
-
-    cout << array_of_points << endl;
-    cout << array_of_points + 1 << endl;
-
-    cout << array_of_points[2].x << " " << array_of_points[2].y << endl;
-    cout << (*(array_of_points+2)).x << " " << (*(array_of_points+2)).y << endl;
-    cout << (array_of_points+2) -> x << " " << (array_of_points+2) -> y << endl;
-
-    node element1, element2;
-    element1.next_node = &element2;
-    element2.value = 5;
-
-    cout << (*element1.next_node).value << endl;
-    cout << element1.next_node -> value << endl;
+    
 
     return 0;
 }
