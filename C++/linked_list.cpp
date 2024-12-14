08a54e533ae25000f0dd9dc7db3fced391b789b2
Demir-B <demir.bibezic@gmail.com>
2024-12-14 18:16:33 +0000

    Made a linked list where I can add new elements, print the list values and hopefully free the memory correctly

--- C++/linked_list.cpp
+++ C++/linked_list.cpp
@@ -4,38 +4,38 @@ using namespace std;
 
 struct node{
     int value;
-    node * next_node;
+    node* next_node;
 };
 
 struct list {
     int length;
-    node * first_node;
+    node* first_node;
 };
 
-node next_node(node current_node) {
-    return *(current_node).next_node;
-}
-
-node create_element(int value) {
-    node element;
-    element.value = value;
+node* create_element(int value) {
+    node* element = (node*) malloc(sizeof(node));
+    element -> value = value;
+    element -> next_node = nullptr;
+    
     return element;
 }
 
 list add_element(list L, int value) {
-    cout << L.length << endl;
-    node element;
-    element = create_element(value);
-    if (L.length == 0) {
-        L.first_node = &element;
+    if (L.length==0) {
+        node* element;
+        element = create_element(value);
+        L.first_node = element;
     }
 
     else if (L.length >= 1) {
-        node current_node = *L.first_node;
+        node* current_node = L.first_node;
         for (int i = 0; i < L.length - 1; i++) {
-            current_node = next_node(current_node);
+            current_node = current_node -> next_node;
         }
-        current_node.next_node = &element;
+        node* element;
+        element = create_element(value);
+        current_node -> next_node = element;
+
     }
 
     L.length += 1;
@@ -51,21 +51,30 @@ void remove_element(int index) {
 }
 
 void print_list(list L) {
-    node current_node = *L.first_node;
-    cout << "Test1" << endl;
-    cout << current_node.value << endl;
-    cout << "Test2" << endl;
-        for (int i = 0; i < L.length - 1; i++) {
-            current_node = next_node(current_node);
-            cout << current_node.value << endl;
-        }
+    node* current_node = L.first_node;
+    cout << current_node -> value << endl;
+    for (int i=0; i<L.length-1; i++) {
+        current_node = current_node -> next_node;
+        cout << current_node -> value << endl;
+    }
+}
+
+void free_all(list L) {
+    node* current_node = L.first_node;
+    node* next_node = current_node -> next_node;
+    free(current_node);
+    for (int i=0; i<L.length-1; i++) {
+        current_node = next_node;
+        next_node = current_node -> next_node;
+        free(current_node);
+    }
 }
 
 int main() {
-    list L = {0,0};
+    list L = {0,nullptr};
     L = add_element(L, 5);
     L = add_element(L, 6);
     print_list(L);
-
+    free_all(L);
     return 0;
 } 
